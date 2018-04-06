f2e55fcd31dccc347fba22d50ec4f9706868e44b
Oregu7 <jekkicool@yandex.ru>
2018-04-06 11:24:15 +0300

    use localtunnel

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-const fs = require("fs");
-const ip = require("ip");
-
-const env = require("config").get("env");
+const localtunnel = require("localtunnel");
+const Koa = require("koa");
+const koaBody = require("koa-body");
+const token = require("config").get("bot.token");
 const bot = require("./app/bot");
-//start bot
-if (env === "development") {
-    bot.telegram.setWebhook();
-    bot.startPolling();
-} else {
-    const port = 8443;
-    // TLS options
-    const tlsOptions = {
-        key: fs.readFileSync("./webhook_pkey.pem"),
-        cert: fs.readFileSync("./webhook_cert.pem"),
-    };
 
-    // Set telegram webhook
-    bot.telegram.setWebhook(`https://${ip.address()}:${port}/bot-mangakun`, {
-        source: fs.readFileSync("./webhook_cert.pem"),
-    });
+const tunnel = localtunnel(3000, function(err, tunnel) {
+    if (err) console.error(err);
 
-    // Start https webhook
-    bot.startWebhook("/bot-mangakun", tlsOptions, port);
-}
+    bot.telegram.setWebhook(`${tunnel.url}/bot-${token}`);
+    const app = new Koa();
+    app.use(koaBody());
+    app.use((ctx, next) => ctx.method === "POST" || ctx.url === `/bot-${token}` ?
+        bot.handleUpdate(ctx.request.body, ctx.response) :
+        next()
+    );
+    app.listen(3000);
+});
+
+tunnel.on("close", function() {
+    console.log("tunnels are closed");
+});
