bf2e65c2bccb6b25dc123ada01ed29cffe11dac5
Oregu7 <jekkicool@yandex.ru>
2018-04-06 14:39:35 +0300

    deploy@0.2.5

--- app/controllers/addFileController.js
+++ app/controllers/addFileController.js
@@ -1,14 +1,11 @@
-const { Mutex } = require("../util");
 const { createFile, sendFile } = require("../helpers").fileManager;
 
-module.exports = (type) => new Mutex(async(ctx, done) => {
+module.exports = (type) => async(ctx) => {
     try {
         //await ctx.reply(ctx.i18n.t("base.addFileCommand"));
         const file = await createFile(ctx, type);
         let ok = await sendFile(ctx, file);
-        return done();
     } catch (err) {
         ctx.reply(ctx.i18n.t("base.fileLoadError"));
-        return done();
     }
-}).start();
+};
